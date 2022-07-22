95ca728b70c49c61b5b063f622ac4fe9ecf29c63
Xolmer <xolmer@Xolmers-MacBook-Pro.local>
2022-07-22 18:45:09 +0200

    footer sidebar

--- components/Sidebar.tsx
+++ components/Sidebar.tsx
@@ -17,11 +17,11 @@ const Sidebar = () => {
   const normalLink = "flex items-center gap-3 hover:bg-neutral-200 p-3 justify-center xl:justify-start cursor-pointer font-semibold text-[#00b5b2] rounded";
   return (
     <div>
-      <div onClick={() => setShowSidebar((prev) => !prev)} className="block xl:hidden m-2 ml-4 mt-3 text-xl">
+      <div onClick={() => setShowSidebar((prev) => !prev)} className="block xl:hidden m-2 ml-4 mt-3 text-xl cursor-pointer">
         {showSidebar ? <ImCancelCircle /> : <AiOutlineMenu />}
       </div>
       {showSidebar && (
-        <div className="xl:w-400 w-30 flex flex-col justify-start mb-10 border-r-2 border-gray-100 xl:border-0 p-3">
+        <div className="xl:w-400 w-20 flex flex-col justify-start mb-10 border-r-2 border-gray-100 xl:border-0 p-3">
           <div className="xl:border-b-2 border-gray-200 xl:pb-4">
             <Link href="/">
               <div className={normalLink}>
