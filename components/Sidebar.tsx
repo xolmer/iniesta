54fe8af06fd897c110574550897ca5b6d8c35f18
Xolmer <xolmer@Xolmers-MacBook-Pro.local>
2022-07-22 17:22:35 +0200

    sidebar setup

--- components/Sidebar.tsx
+++ components/Sidebar.tsx
@@ -1,7 +1,66 @@
-import React from "react";
+import { useState } from "react";
+import { NextPage } from "next";
+import { useRouter } from "next/router";
+import Link from "next/link";
+import GoogleLogin from "react-google-login";
+import { AiFillHome, AiOutlineMenu } from "react-icons/ai";
+import { ImCancelCircle } from "react-icons/im";
+import Discover from "./Discover";
+import SuggestedAccounts from "./SuggestedAccounts";
+import Footer from "./Footer";
 
 const Sidebar = () => {
-  return <div>Sidebar</div>;
+  const [showSidebar, setShowSidebar] = useState(true);
+
+  const [userProfile, setUserProfile] = useState(false);
+
+  const normalLink = "flex items-center gap-3 hover:bg-neutral-200 p-3 justify-center xl:justify-start cursor-pointer font-semibold text-[#00b5b2] rounded";
+  return (
+    <div>
+      <div onClick={() => setShowSidebar((prev) => !prev)} className="block xl:hidden m-2 ml-4 mt-3 text-xl">
+        {showSidebar ? <ImCancelCircle /> : <AiOutlineMenu />}
+      </div>
+      {showSidebar && (
+        <div className="xl:w-400 w-30 flex flex-col justify-start mb-10 border-r-2 border-gray-100 xl:border-0 p-3">
+          <div className="xl:border-b-2 border-gray-200 xl:pb-4">
+            <Link href="/">
+              <div className={normalLink}>
+                <p className="text-2xl">
+                  <AiFillHome />
+                </p>
+                <span className="text-xl hidden xl:block">For You</span>
+              </div>
+            </Link>
+          </div>
+          {!userProfile && (
+            <div className="px-2 py-4 hidden xl:block">
+              <p className="text-gray-400">Log in to like and comment on videos</p>
+              <div className="pr-4">
+                <GoogleLogin
+                  clientId=""
+                  onSuccess={() => {}}
+                  onFailure={() => {}}
+                  cookiePolicy={"single_host_origin"}
+                  render={(renderProps) => (
+                    <button
+                      className=" cursor-pointer bg-white text-lg text-[#00b5b2] border-[1px] border-[#00b5b2] font-semibold px-6 py-3 rounded-md outline-none w-full mt-3 hover:text-white hover:bg-[#00b5b2]"
+                      onClick={renderProps.onClick}
+                      disabled={renderProps.disabled}
+                    >
+                      Log in
+                    </button>
+                  )}
+                />
+              </div>
+            </div>
+          )}
+          <Discover />
+          <SuggestedAccounts />
+          <Footer />
+        </div>
+      )}
+    </div>
+  );
 };
 
 export default Sidebar;
